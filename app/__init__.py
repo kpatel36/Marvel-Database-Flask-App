ba8c8249d0958aaef980e858bba350ff3ac88ca1
kpatel36 <k.patel0058@gmail.com>
2022-03-20 23:11:45 -0500

    add in databases and login manager

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,21 @@ from config import Config
 
 from .auth.authroutes import auth
 
+from .models import db, login
+from flask_migrate import Migrate
+
 app=Flask(__name__)
 
 app.config.from_object(Config)
 
 app.register_blueprint(auth)
 
+
+db.init_app(app) 
+migrate = Migrate(app,db)
+
+login.init_app(app)
+
+
 from . import routes
+from . import models
---
+++ app/models.py
@@ -0,0 +1,41 @@
+# this models.py file is responsible for everything database
+# primarily instantiation of ORM and create of database models (tables/entities)
+
+# import ORM
+from flask_sqlalchemy import SQLAlchemy
+
+from flask_login import LoginManager, UserMixin 
+
+
+# create instance of ORM
+# practice to refer to database as db in instantiation
+db = SQLAlchemy()
+
+login=LoginManager()
+
+# necarry function for our login manager
+@login.user_loader
+def load_user(user_id):
+    return User.query.get(user_id) 
+
+
+from datetime import datetime, timezone
+from werkzeug.security import generate_password_hash
+from uuid import uuid4
+
+
+# create database model 
+class User(db.Model, UserMixin):
+    id = db.Column(db.String(50), primary_key=True)
+    username = db.Column(db.String(25), nullable=False, unique=True)
+    email = db.Column(db.String(60), nullable=False, unique=True)
+    displayname = db.Column(db.String(50), nullable=False, unique=True)
+    password = db.Column(db.String(250), nullable=False)
+    data_created = db.Column(db.DateTime, default=datetime.now(timezone.utc))
+
+    def __init__(self, username, email, password, displayname):
+        self.username = username
+        self.email = email.lower()
+        self.displayname = displayname.lower()
+        self.password = generate_password_hash(password)
+        self.id = str(uuid4())
